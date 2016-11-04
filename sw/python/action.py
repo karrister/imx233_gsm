26c64d605b1ebc9c561a2609a88704ca1ebaf171
Karri Kivela <karrister@gmail.com>
2016-11-04 23:48:07 +0200

    Adding logger, action *Adding a class for logging events *Adding a class for actions

---
+++ sw/python/action.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Author: 
+#    Karri Kivela
+#
+# Description:
+#    This is the file for actions.
+
+class Action:
+
+    isInErrorState = False
+	#nextAction = 
+	incomingCall = False
+	
+
+    def __init__(self):
+        print "Only a stub for now"
+
+	
---
+++ sw/python/logger.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# Author: 
+#    Karri Kivela
+#
+# Description:
+#    This is the file for logging the operation.
+
+LOG_INFO = 0
+
+class Logger:
+
+    isInErrorState = False	
+
+    def __init__(self):
+        print "For now the logger supports only printing the message"
+		
+		
+	def pushLogMsg(string, verbosity = LOG_INFO):
+		print "V" + verbosity + ": " + string
+
+	
